a96056ca181a895fc6260a1f054647140fbb0f77
michaelbir <mikux22@gmail.com>
2023-05-03 21:36:12 +0300

    first commit

---
+++ Random_element/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] snacks = { "sushi", "pitza", "chips", "chicken", "pepsi", "coca" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are eat{snacks[randomIndex]}.");
+
+
---
+++ Week_6/Program.cs
@@ -0,0 +1,23 @@
+﻿// string [] fruit = {"apples","bananas","oranges"]
+
+string[] fruit = new string[3];
+
+fruit[0] = "apples";
+fruit[1] = "bananas";
+fruit[2] = "oranges";
+
+Console.WriteLine($"{fruit[0]},{fruit[2]},{fruit[3]}");
+
+int fruitArreyLenght = fruit.Length;
+
+Console.WriteLine($"There are {fruitArreyLenght} in your array. ");
+
+for (int i = 0; i < fruitArreyLenght; i++)
+{
+    fruit[i] = fruit[i].Replace(fruit[i][0], char.ToUpper(fruit[i][0]));
+}
+
+foreach (string elemet in fruit)
+{
+    Console.WriteLine(elemet);
+}
